1a67b60c1460d7567b243353e1954c6d53ceb5a5
wj5403293 <168689837+wj5403293@users.noreply.github.com>
2024-10-30 18:30:39 +0800

    Update strip.sh

--- strip.sh
+++ strip.sh
@@ -3,15 +3,15 @@
 # 指定要处理的目录路径
 TARGET_DIR="./install/lib"
 
-# 遍历指定目录下所有 .a 文件
-for file in "$TARGET_DIR"/*.a; do
-    # 检查是否有 .a 文件
+# 遍历指定目录下所有 .a 和 .so 文件
+for file in "$TARGET_DIR"/*.{a,so}; do
+    # 检查文件是否存在
     if [ -f "$file" ]; then
         echo "正在处理文件: $file"
         # 对文件进行 strip 操作
         $HOME/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip "$file"
         echo "已完成: $file"
     else
-        echo "没有找到 .a 文件"
+        echo "没有找到 .a 或 .so 文件"
     fi
 done
